e8574b11e514afb850b68a9a3b44de50470fa91b
kiwonbyun <bkw9603@naver.com>
2023-02-17 19:46:40 +0900

    cors해결

--- app.js
+++ app.js
@@ -10,8 +10,19 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { sendMail, genExcel, getIndex } = require('./func');
 
+const whitelist = [
+  'http://localhost:3000',
+  'http://stock-mailer.s3-website.ap-northeast-2.amazonaws.com/',
+];
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      // 만일 whitelist 배열에 origin인자가 있을 경우
+      callback(null, true); // cors 허용
+    } else {
+      callback(new Error('Not Allowed Origin!')); // cors 비허용
+    }
+  },
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
